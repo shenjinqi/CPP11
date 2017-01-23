2d39338704e4f206d54b86d08548ffeb4e8751c6
shenjinqi <>
2017-01-23 14:27:42 +0800

    initial c++ hello word

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: shenjinqi
+ *
+ * Created on 2017年1月23日, 上午11:43
+ */
+
+#include <cstdlib>
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    std::cout<<"Hello World\n";
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=cpp11_step_by_step
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cpp11_step_by_step
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=cpp11stepbystep.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cpp11stepbystep.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=cpp11_step_by_step
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/cpp11_step_by_step
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=cpp11stepbystep.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/cpp11stepbystep.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
